944a68a0892ce434fc5e8b80b681839f91b24cc6
Leo Souza <leosouza221@gmail.com>
2019-10-21 17:49:56 -0300

    Initial version of main page

--- src/app/Main.vue
+++ src/app/Main.vue
@@ -1,3 +1,20 @@
 <template lang="pug">
-  span Welcome to my vue
+  .main-page
+    v-img(
+      src="@/../public/images/FundoTela.png"
+      height="100%"
+    )
+      v-layout(justify-center align-center fill-height)
+        v-card(width="90%" height="50%")
+          span testes
 </template>
+
+<style>
+  .main-page {
+    width: 100vw;
+    height: 100vh;
+  }
+  .teste {
+    background: rgb(0, 0, 0, 0.8);
+  }
+</style>
