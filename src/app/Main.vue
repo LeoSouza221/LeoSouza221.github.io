9995bdf03803057eb94a1bc0bdf2512bea508220
Leo Souza <leosouza221@gmail.com>
2019-10-30 17:51:06 -0300

    Added images and adjusted your position

--- src/app/Main.vue
+++ src/app/Main.vue
@@ -1,46 +1,79 @@
 <template lang="pug">
   .main-page
-    v-navigation-drawer(
-      app
-      fixed
-      dark
-      permanent
-      style="background: linear-gradient(120deg, #00E676, #B9F6CA); box-shadow: 0px 15px 5px grey;"
-    )
     v-content
       header.default-header.default-header-main
         div.avatar
-      v-container(style="height: 100%;")
+      v-container(style="height: 75%;")
         v-layout(justify-center row wrap align-center fill-height)
-          v-flex(xs12 sm4)
-            v-card.elevation-1.ma-5
+          v-flex(xs12 sm6)
+            v-card.elevation-1.mx-3
               v-img(
                 class="white--text"
                 gradient="to bottom, rgba(0,0,0,.8), rgba(0,0,0,.5)"
                 src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                max-height="250px"
+                height="250px"
               )
                 v-card-title Apresentacao
                 v-card-subtitle.white--text Olá, me chamo Leonardo, sou formado em Análise e Desenvolvimento de Sistemas desde o comeco de 2019. Ja fui estagiario de BI por cerca de um ano e meio, onde era responsvel por criar os dashboards e aplicar as metricas pre definidas
                 v-card-subtitle.white--text Atualmente trabalho como desenvolvedor front-end, utilizando VueJS com Vuetify e buscando sempre novos conhecimentos e aprender coisas novas.
-          v-flex(xs12 sm3)
-            v-card.elevation-1.ma-5
+          v-flex(xs12 sm4)
+            v-card.elevation-1.m-3
               v-img(
                 class="white--text"
                 gradient="to bottom, rgba(0,0,0,.8), rgba(0,0,0,.5)"
                 src="https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                max-height="250px"
+                height="250px"
               )
                 v-card-title Conhecimentos
-          v-flex(xs12 sm7)
-            v-card.elevation-1.ma-5
+                div.my-grid.my-grid-knowledge
+                  div.my-grid-item
+                    img(
+                      src="@/../public/images/css.png"
+                      width="50"
+                      height="50"
+                    )
+                  div.my-grid-item
+                    img(
+                      src="@/../public/images/html.png"
+                      width="50"
+                      height="50"
+                    )
+                  div.my-grid-item 
+                    img(
+                      src="@/../public/images/js.png"
+                      width="50"
+                      height="50"
+                    )
+                  div.my-grid-item
+                    img(
+                      src="@/../public/images/vue.png"
+                      width="50"
+                      height="50"
+                    )
+          v-flex(xs12 sm10)
+            v-card.elevation-1.mx-3
               v-img(
                 class="white--text"
                 gradient="to bottom, rgba(0,0,0,.8), rgba(0,0,0,.5)"
                 src="https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80"
-                max-height="100px"
+                max-height="150px"
               )
                 v-card-title Contatos e Referencias
+                div.my-grid.my-grid-contacts
+                  div.my-grid-item
+                    a
+                      img(
+                        src="@/../public/images/git.png"
+                        width="80"
+                        height="80"
+                      )
+                  div.my-grid-item
+                    a
+                      img(
+                        src="@/../public/images/linkedin.png"
+                        width="60"
+                        height="60"
+                      )
         v-slide-y-transition(mode="out-in")
           router-view
 </template>
@@ -79,9 +112,23 @@ export default {
    .main-page {
      height: 100vh;
      width: 100vw;
-     background: #00E676;
    }
-   .borda {
-     border: solid 5px #00E676;
+   .my-grid {
+     display: grid;
+     padding: 10px;
+     width: 100%;
+     height: 60%;
+   }
+   .my-grid-knowledge {
+     grid-template-columns: repeat(3, 1fr);
+     grid-template-rows: repeat(2, 1fr);
+   }
+   .my-grid-contacts {
+     grid-template-columns: repeat(2, 1fr);
+   }
+   .my-grid-item {
+     display: flex;
+     justify-content: center;
+     align-items: center;
    }
 </style>
---
+++ src/app/core/SideBar.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .sidebar
+    v-navigation-drawer(
+      app
+      fixed
+      dark
+      permanent
+      style="background: linear-gradient(120deg, #00E676, #B9F6CA); box-shadow: 0px 15px 5px grey;"
+    )
+    //- v-list(rounded dense elevation-3)
+    //-   template(v-for="(item, index) in sideBarRoutes")
+    //-     v-list-group(
+    //-       v-if="item.children && item.children[0].meta"
+    //-       :group="item.path"
+    //-       :prepend-icon="item.icon"
+    //-       :expand="true"
+    //-       :key="index"
+    //-     )
+    //-       template(v-slot:activator)
+    //-         v-list-item-content
+    //-           v-list-item-title {{ item.title }}
+    //-       v-list-item(
+    //-         v-for="(subItem, y) in item.children"
+    //-         :key="y"
+    //-         :to="subItem.path"
+    //-       )
+    //-         v-list-item-action
+    //-           v-icon(v-html="subItem.meta.sidebar.icon")
+    //-         v-list-item-content
+    //-           v-list-item-title(v-text="subItem.meta.sidebar.title")
+    //-     v-list-tile(
+    //-       v-else
+    //-       ripple
+    //-       :key="index"
+    //-       :to="item.path"
+    //-     )
+    //-       v-list-item-icon
+    //-         v-icon(v-html="item.icon")
+    //-       v-list-item-content
+    //-         v-list-item-title(v-text="item.title")
+</template>
