46c0e195ad37d3d635ec2775e62a9f1f6035449f
LeoSouza221 <leosouza221@gmail.com>
2019-11-19 21:14:02 -0300

    Added snackbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   v-app.background
     v-fade-transition(mode="out-in")
       router-view
+    v-snackbar(
+      v-model="snackbar"
+      :color="color"
+      :timeout="12000"
+      top
+    ) 
+      v-icon(dark left) fa fa-exclamation-triangle 
+      span {{ text }}
     //- v-dialog(v-model="dialog" persistent max-width="200")
     //-   v-card(flat height="200")
     //-     div(style=`display: flex;
@@ -14,7 +22,27 @@
 </template>
 
 <script>
+import { EventBus } from '@/app/core/event-bus';
+
 export default {
+  data: () => ({
+    snackbar: false,
+    color: null,
+    text: null,
+  }),
+  created() {
+    EventBus.$on('show-snackbar', this.openSnackbar);
+  },
+  beforeDestroy() {
+    EventBus.$off('show-snackbar', this.openSnackbar);
+  },
+  methods: {
+    openSnackbar(color, msg) {
+      this.snackbar = true;
+      this.color = color;
+      this.text = msg;
+    }
+  },
   name: 'App',
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import routes from './routes';
+import { EventBus } from '@/app/core/event-bus';
 
 Vue.use(Router);
 
@@ -13,6 +14,16 @@ const router = new Router({
       children: routes,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (!localStorage.getItem('snackbar') && to.name !== 'Main') {
+    EventBus.$emit('show-snackbar', 'secondary', `
+      Todos os layouts sao exemplos e nao condizem com um projeto real 
+    `);
+    localStorage.setItem('snackbar', true);
+  }
+  next();
 });
 
 export default router;
