13cbc74b74e2fd88159ff610109b5e87523158ab
LeoSouza221 <leosouza221@gmail.com>
2020-02-14 12:09:17 -0300

    component  separated in parts

--- src/app/layouts/ecommerce/LayoutEcommerce.vue
+++ src/app/layouts/ecommerce/LayoutEcommerce.vue
@@ -1,85 +1,20 @@
 <template lang="pug">
   .e-commerce
     v-card(width="100%" height="100%")
-      v-toolbar(
-        flat
-        style="border-bottom: solid 1px grey"
-      )
-        v-row(
-          justify="center"
-          align="center"
-        )
-          v-col(cols="2" style="display: inline-flex")
-            v-icon.mr-3 fa fa-store
-            h1 Loja
-            
-          v-col(cols="2")
-            v-menu(offset-y)
-              template(v-slot:activator="{ on }")
-                v-btn(
-                  v-on="on"
-                  light
-                  text
-                ) Departamentos
-              v-list
-                v-list-item(
-                  v-for="(item, index) in items"
-                  :key="index"
-                  @click=""
-                )
-                  v-list-item-icon
-                    v-icon {{ item.icon }}
-                  v-list-item-title {{ item.title }}
-          v-col(cols="4")
-            v-text-field.mt-4(
-              label="Pesquisar"
-              append-icon="fa fa-search"
-            )
-          v-col(cols="4")
-            v-row(justify="end")
-              v-menu(offset-y open-on-hover)
-                template(v-slot:activator="{ on }")
-                  v-avatar.mr-4(v-on="on" style="border: solid 1px grey; border-radius: 50%")
-                    v-icon fa fa-user
-                v-list
-                  v-list-item
-                    v-list-item-icon
-                      v-icon far fa-user
-                    v-list-item-title Perfil
-                  v-list-item
-                    v-list-item-icon
-                      v-icon fas fa-sign-out-alt
-                    v-list-item-title Sair
-      v-navigation-drawer(
-        v-model="drawer"
-        light
-        color="white"
-        height="calc(100% - 64px)"
-      )
-        h2 Filtros
-        v-list-group(
-          v-for="item in itemsNavbar"
-          :key="item.title"
-          v-model="item.active"
-          no-action
-        )
-        
-          template(v-slot:activator)
-            v-list-item-content
-              v-list-item-title(v-text="item.title")
-          v-list-item(
-            v-for="subItem in item.items"
-            :key="subItem.title"
-            @click="")
-          
-            v-list-item-content
-              v-list-item-title(v-text="subItem.title")
+      Toolbar(:items="items")
+      Navbar(:itemsNavbar="itemsNavbar")
     
 </template>
   
 <script>
+import Toolbar from './ToolbarEcommerce';
+import Navbar from './NavbarEcommerce';
 
 export default {
+  components: {
+    Toolbar,
+    Navbar,
+  },
   data: () => ({
     drawer: true,
     items: [
@@ -101,39 +36,43 @@ export default {
       },
     ],
     itemsNavbar: [
-      {
-        title: 'Preco',
-        items: [
-          { title: 'Breakfast & brunch' },
-          { title: 'New American' },
-          { title: 'Sushi' },
-        ],
+      { header: 'Preco' },
+      { 
+        title: 'Breakfast & brunch',
+        type: 'slide'
       },
-      {
-        title: 'Frete',
-        items: [
-          { title: 'Gratis' },
-          { title: 'ate 50 reais' },
-          { title: 'ate 100 reais' },
-        ],
+      { divider: true },
+      { header: 'Frete' },
+      { 
+        title: 'Gratis',
+        type: 'checkbox',
+        min: 0,
+        max: 1000,
+        range: [0, 1000]
       },
-      {
-        title: 'Marca',
-        active: true,
-        items: [
-          { title: 'Marca 1' },
-          { title: 'Marca 2' },
-          { title: 'Marca 3' },
-        ],
+      { 
+        title: 'ate 50 reais',
+        type: 'checkbox'
       },
-      {
-        title: 'Eletrodomesticos',
-        items: [
-            { title: 'Breakfast & brunch' },
-            { title: 'New American' },
-            { title: 'Sushi' },
-        ],
+      { 
+        title: 'ate 100 reais',
+        type: 'checkbox'
       },
+      { divider: true },
+      { header: 'Marca' },
+      { 
+        title: 'Marca 1',
+        type: 'checkbox'
+      },
+      { 
+        title: 'Marca 2',
+        type: 'checkbox'
+      },
+      { 
+        title: 'Marca 3',
+        type: 'checkbox'
+      },
+      { divider: true },
     ]
   }),
   name: 'LayoutEcommerce',
@@ -144,5 +83,9 @@ export default {
   .e-commerce {
     width: 100%;
     height: 100%;
+  }
+  .v-input--selection-controls {
+    margin: 0;
+    padding: 0;
   }
 </style>
---
+++ src/app/layouts/ecommerce/ToolbarEcommerce.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  v-toolbar(
+    flat
+    style="border-bottom: solid 1px grey"
+  )
+    v-row(
+      justify="center"
+      align="center"
+    )
+      v-col(cols="2" style="display: inline-flex")
+        v-icon.mr-3 fa fa-store
+        h1 Loja
+      v-col(cols="2")
+        v-menu(offset-y)
+          template(v-slot:activator="{ on }")
+            v-btn(
+              v-on="on"
+              light
+              text
+            ) Departamentos
+          v-list
+            v-list-item(
+              v-for="(item, index) in items"
+              :key="index"
+              @click=""
+            )
+              v-list-item-icon
+                v-icon {{ item.icon }}
+              v-list-item-title {{ item.title }}
+      v-col(cols="4")
+        v-text-field.mt-4(
+          label="Pesquisar"
+          append-icon="fa fa-search"
+        )
+      v-col(cols="4")
+        v-row(justify="end")
+          v-menu(offset-y open-on-hover)
+            template(v-slot:activator="{ on }")
+              v-avatar.mr-4(v-on="on" style="border: solid 1px grey; border-radius: 50%; cursor: pointer")
+                v-icon fa fa-user
+            v-list
+              v-list-item
+                v-list-item-icon
+                  v-icon far fa-user
+                v-list-item-title Perfil
+              v-list-item
+                v-list-item-icon
+                  v-icon fas fa-sign-out-alt
+                v-list-item-title Sair
+</template>
+
+<script>
+export default {
+  name: 'ToolbarEcommerce',
+  props: ['items'],
+}
+</script>
