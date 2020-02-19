fe917139bce1961286d67d37f5aab0b0723157d7
LeoSouza221 <leosouza221@gmail.com>
2020-02-19 16:19:08 -0300

    finished the initial version of the draw

--- src/app/minhasLoucuras/desenho/Anime.vue
+++ src/app/minhasLoucuras/desenho/Anime.vue
@@ -2,13 +2,19 @@
   .desenho
     .personagem
       .rosto-cima
+        .sombra-rosto-cima
       .rosto-meio
+        .sombra-rosto-meio
       .rosto-baixo
+        .sombra-rosto-baixo
       .queixo
+        .queixo-sombra
       .orelha-direita
         .ouvido.direito
+        .sombra-orelha.direito
       .orelha-esquerda
         .ouvido.esquerdo
+        .sombra-orelha.esquerdo
       .olhos
         .olho-direito
           .iris
@@ -17,16 +23,37 @@
       .nariz
       .traco
       .boca
+    //- .luz
     .fundo
+
 </template>
 
 <style>
+/* --------------------------------------*/
   .desenho {
     width: 100%;
     height: 100%;
-    background-color: grey;
+    background-color: black;
     overflow: hidden;
     position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+/* --------------------------------------*/
+
+  .fundo {
+    width: 800px;
+    height: 800px;
+    position: absolute;
+    background-image: radial-gradient(circle, #e6ff43, #eaff67, #e9f596, #fff, rgba(255, 255, 255, 1));
+    top: 0;
+    box-shadow:
+      0 0 60px 30px #fff,
+      0 0 100px 60px #eaff67,
+      0 0 140px 90px rgb(233, 228, 180);
+    border-radius: 50%;
   }
   
   .personagem {
@@ -34,16 +61,28 @@
     height: 100%;
     margin: 0 auto;
     position: relative;
+    z-index: 1;
   }
   
   .rosto-cima {
     width: 500px;
     height: 300px;
-    background-color: white;
+    background-color: rgb(255, 255, 231);
     position: absolute;
     border-radius: 500px 500px 100px 100px;
-    border: 3px solid black;
+    border-left: 1px solid black;
     top: 150px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .sombra-rosto-cima {
+    width: 450px;
+    height: 285px;
+    background: rgb(94, 94, 94);
+    position: absolute;
+    border-radius: 500px 500px 100px 100px;
+    bottom: 0;
     left: 50%;
     transform: translateX(-50%);
   }
@@ -51,7 +90,7 @@
   .rosto-meio {
     width: 500px;
     height: 150px;
-    background-color: white;
+    background-color: rgb(255, 255, 231);
     position: absolute;
     top: 400px;
     left: 50%;
@@ -59,29 +98,31 @@
     clip-path: polygon(0 0, 100% 0, 96% 100%, 4% 100%);
   }
 
+  .sombra-rosto-meio {
+    width: 450px;
+    height: 100%;
+    background: rgb(94, 94, 94);
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    clip-path: polygon(0 0, 100% 0, 96% 100%, 4% 100%);
+
+  }
+
   .rosto-meio::before {
     content: "";
     position: absolute;
     height: 100%;
-    border: 2px solid black;
+    border: 1px solid black;
     left: 9px;
     transform: rotate(-8deg);;
-  }
-
-  .rosto-meio::after {
-    content: "";
-    position: absolute;
-    height: 100%;
-    border: 2px solid black;
-    right: 10px;
-    transform: rotate(8deg);
   }
 
   .rosto-baixo {
     width: calc(500px - 4.5%);
     height: 150px;
     position: absolute;
-    background-color: white;
+    background-color: rgb(255, 255, 231);
     top: 548px;
     left: 50%;
     transform: translateX(-50%);
@@ -89,21 +130,22 @@
     z-index: 1;
   }
 
-  .rosto-baixo::after {
-    content: "";
+  .sombra-rosto-baixo {
+    width: calc(435px - 4.5%);
+    height: 100%;
     position: absolute;
-    height: 200px;
-    border: 2px solid black;
-    right: 50px;
-    top: -20px;
-    transform: rotate(31.5deg);
+    background: rgb(94, 94, 94);
+    left: 50%;
+    transform: translateX(-50%);
+    clip-path: polygon(0 0, 100% 0, 75% 100%, 25% 100%);
+    z-index: 1;
   }
 
   .rosto-baixo::before {
     content: "";
     position: absolute;
     height: 200px;
-    border: 2px solid black;
+    border: 1px solid black;
     left: 49px;
     top: -20px;
     transform: rotate(-31.5deg);
@@ -113,48 +155,58 @@
     width: calc(500px - 24.5%);
     height: 150px;
     position: absolute;
-    background-color: white;
+    background-color: rgb(255, 255, 231);
+    position: absolute;
     top: 610px;
     left: 50%;
     transform: translateX(-50%);
     border-radius: 0 0 50% 50%;
-    border-left: 3px solid black;
-    border-right: 3px solid black;
-    border-bottom: 3px solid black;
+    border-left: 1px solid black;
+    border-bottom: 1px solid black;
+    z-index: 0;
+  }
+
+  .queixo-sombra {
+    width: calc(295px - 24.5%);
+    height: 125px;
+    position: absolute;
+    background: rgb(94, 94, 94);
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 0 0 50% 50%;
     z-index: 0;
   }
 
   .orelha-direita {
     width: 200px;
     height: 200px;
-    background-color: white;
+    background-color: rgb(255, 255, 231);
     position: absolute;
     top: 45px;
     right: 148px;
     transform: rotate(38deg);
-    border-left: 3px solid black;
-    border-right: 3px solid black;
-    border-top: 3px solid black;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
     border-radius: 120% 700% 0 0 / 500% 700%  50% 50%;
   }
 
   .orelha-esquerda {
     width: 200px;
     height: 200px;
-    background-color: white;
+    background-color: rgb(255, 255, 231);
     position: absolute;
     top: 45px;
     left: 148px;
     transform: rotate(-38deg);
-    border-left: 3px solid black;
-    border-right: 3px solid black;
-    border-top: 3px solid black;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
     border-radius: 700% 120% 0 0 / 700% 500% 50% 50%;
   }
 
   .ouvido {
-    width: 150px;
-    height: 180px;
+    width: 130px;
+    height: 130px;
     position: absolute;
     bottom: 0;
     background-color: rgb(172, 102, 125);
@@ -162,6 +214,17 @@
     transform: translateX(-50%);
     box-shadow: inset 0 20px 10px rgb(51, 51, 51),
       inset 0 -3px 10px rgb(51, 51, 51);
+    z-index: 1;
+  }
+
+  .sombra-orelha {
+    width: 160px;
+    height: 160px;
+    position: absolute;
+    bottom: 0;
+    background: rgb(94, 94, 94);
+    left: 50%;
+    transform: translateX(-50%);
   }
 
   .esquerdo {
@@ -189,7 +252,7 @@
     background-color: rgb(15, 15, 15);
     border-radius: 50% 0 50% 0;
     transform: rotate(20deg);
-    box-shadow: inset 7px 0px 5px rgb(112, 112, 112);
+    box-shadow: inset 7px 0 15px rgb(112, 112, 112), inset 0 7px 12px rgb(179, 179, 179);
   }
 
   .olho-esquerdo {
@@ -200,12 +263,12 @@
     background-color: rgb(15, 15, 15);
     border-radius: 0 50% 0 50%;
     transform: rotate(-20deg);
-    box-shadow: inset -7px 0px 5px rgb(112, 112, 112);
+    box-shadow: inset -7px 0 15px rgb(112, 112, 112), inset 0 7px 12px rgb(179, 179, 179);
   }
 
   .iris {
-    width: 20px;
-    height: 20px;
+    width: 30px;
+    height: 30px;
     position: absolute;
     background-color: rgb(202, 202, 202);
     top: 50%;
@@ -247,6 +310,43 @@
     z-index: 2;
     border-radius: 50% 50% 0 0;
     border-top: 5px solid rgb(15, 15, 15);
+  }
+
+/*--------------------------------------------------------*/
+
+  .luz {
+    width: 10px;
+    height: 10px;
+    background-color: yellow;
+    position: absolute;
+    top: 0;
+    left: 10px;
+    border-radius: 50%;
+    animation: down 20s infinite linear;
+    z-index: 2px;
+  }
+  
+  @keyframes down {
+    15% {
+      top: 10%;
+    }
+    45% {
+      left: 10%;
+    }
+    50% {
+      left: 10%;
+    }
+    100% {
+      left: 0;
+      top: 90%;
+      opacity: 0;
+    }
+    0% {
+      opacity: 0;
+    }
+    10% {
+      opacity: 1;
+    }
   }
 
 </style>
