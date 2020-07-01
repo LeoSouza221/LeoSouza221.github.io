4272565f4c870c74b1b649dad4fbdedbc5beaac5
Leonardo Felipe de Souza <39174217+LeoSouza221@users.noreply.github.com>
2020-07-01 20:33:05 -0300

    Add files via upload

---
+++ js/chunk-2d0c9158.fed04368.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c9158"],{5843:function(n,t,e){"use strict";e.r(t);var i=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("v-slide-y-transition",{attrs:{mode:"out-in"}},[e("router-view")],1)},r=[],o=e("2877"),s=e("6544"),u=e.n(s),l=e("0789"),a={},c=Object(o["a"])(a,i,r,!1,null,null,null);t["default"]=c.exports;u()(c,{VSlideYTransition:l["f"]})}}]);
+//# sourceMappingURL=chunk-2d0c9158.fed04368.js.map
